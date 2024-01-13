3e8c1684e623769d19dc5130f7d13838ebd7c6e3
Thouseef <thousithouseef731@gmail.com>
2024-01-13 18:00:08 +0530

    ChatGPT Integration

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +125,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+OPENAI_APIKEY=os.getenv("OPENAI_APIKEY")
--- chats/views.py
+++ chats/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.conf import settings
+from openai import OpenAI
 
+client = OpenAI(api_key=settings.OPENAI_APIKEY)
 # Create your views here.
+
+
+def ask_openai(message):
+    response = client.completions.create(
+        model="tts-1",
+        prompt=message,
+    )
+    print(response)
+    answer = response.choices[0].text.strip()
+    
+    return answer
+
+
+def chatbot(request):
+    if request.method == "POST":
+        message = request.POST.get("message")
+        response = ask_openai(message)
+        return JsonResponse({"message": message, "response": response})
+    return render(request, "chatbot.html")
